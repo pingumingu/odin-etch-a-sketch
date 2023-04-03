8928964b3c535b270aef455878b7b397c13743f5
Ming Hong <68141634+pingumingu@users.noreply.github.com>
2023-04-03 16:39:30 +1000

    Add button for how many squares

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-gridContainer = document.querySelector(".grid");
+let numberOfSquares = 16;
 
-for (let i=0; i<16; i++) {
-    let div = document.createElement('div');
-    div.classList.add('row');
-    gridContainer.appendChild(div);
-}
 
-rows = document.querySelectorAll(".row");
 
-for (row of rows) {
-    for (let i=0; i<16; i++) {
+let createBoxes = function(numberOfSquares) {
+
+    gridContainer = document.querySelector(".grid");
+
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    for (let i=0; i<numberOfSquares; i++) {
         let div = document.createElement('div');
-        div.classList.add('column');
-        div.style['height'] = '40px';
-        div.style['width'] = '40px';
-        div.style['backgroundColor'] = 'purple';
-        row.appendChild(div);
+        div.classList.add('row');
+        gridContainer.appendChild(div);
+    }
 
-        div.addEventListener('mouseover', (function(div) {
-            return function() {
-                div.style['backgroundColor'] = 'black';
-            }
-        })(div))
+    rows = document.querySelectorAll(".row");
+    
+    for (row of rows) {
+        while (row.firstChild) {
+            row.removeChild(row.firstChild)
+        }
+        for (let i=0; i<numberOfSquares; i++) {
+            let div = document.createElement('div');
+            div.classList.add('column');
+            div.style['height'] = `${640/numberOfSquares}px`;
+            div.style['width'] = `${640/numberOfSquares}px`;
+            div.style['backgroundColor'] = 'purple';
+            row.appendChild(div);
+    
+            div.addEventListener('mouseover', (function(div) {
+                return function() {
+                    div.style['backgroundColor'] = 'black';
+                }
+            })(div));
+        }
     }
 }
+
+createBoxes(numberOfSquares)
+
+const sideDimensionButton = document.querySelector('#dimension-button');
+sideDimensionButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    inputField = document.querySelector('#number');
+    numberOfSquares = parseInt(inputField.value);
+    createBoxes(numberOfSquares);
+})
+
+
+
+
+
+
+
 
